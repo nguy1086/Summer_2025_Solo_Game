6a1fbd5f9d3269ec182cd68e86c4554426cb865e
Brendon Nguyen <nguy1086@algonquinlive.com>
2025-04-18 16:13:18 -0400

    setting up

--- Radioactive_Spire/Source/Radioactive_Spire/Radioactive_Spire.Build.cs
+++ Radioactive_Spire/Source/Radioactive_Spire/Radioactive_Spire.Build.cs
@@ -8,7 +8,7 @@ public class Radioactive_Spire : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Paper2D", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
