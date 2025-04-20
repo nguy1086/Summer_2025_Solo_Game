20049055088153cd2a8f8dffd051c97ec43f1b09
Brendon Nguyen <nguy1086@algonquinlive.com>
2025-04-19 21:52:35 -0400

    need to implement some basic character classes and world before testing walk and idle animations

---
+++ Radioactive_Spire/Source/Radioactive_Spire/Players/PlayableCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "PlayableCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RADIOACTIVE_SPIRE_API APlayableCharacter : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
